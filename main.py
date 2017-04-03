0d7a2cef3022a7580b675ff32fcfb4cadfb9d8d5
kraft77 <kraft77@o2.pl>
2017-04-03 11:55:17 +0200

    dodano zapis do pliku DICOM

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
+import skimage
 from skimage import data
 import matplotlib.pyplot as plt
+from dicom.dataset import Dataset, FileDataset
+import datetime, time
 
 step = 2
 detectorsNumber = 120
@@ -86,6 +89,52 @@ def inverseRadonTransform(input, stepSize=1, stepsArray=None, detectorsWidth=140
 
     return output
 
+def saveDicomFile(filename, patientName, patientId, gender, birthday, imageArray, transpose=False):
+
+    meta = Dataset()
+    SOPClassUID = "1.2.840.10008.5.1.4.1.1.2" # sop class UID dla obrazow CT
+    meta.MediaStorageSOPClassUID = SOPClassUID  # Wygenerowany unikalny UID
+    date=datetime.datetime.now().strftime('%Y%m%d') # Obecny czas
+    time=datetime.datetime.now().strftime('%H%M%S.%f') # Obecny czas
+    randomUId = SOPClassUID + "."+date+time # Wygenerowany unikalny UID
+    meta.MediaStorageSOPInstanceUID = randomUId # Wygenerowany unikalny UID
+    meta.ImplementationClassUID = randomUId+"."+"1" # Wygenerowany unikalny UID
+
+    dataSet = FileDataset(filename, {}, file_meta=meta, preamble=b"\0"*128) # Utworzenie obiektu DICOM
+    dataSet.PatientName = patientName   # Imie pacjenta
+    dataSet.PatientID=patientId # Id pacjenta
+    dataSet.PatientBirthDate = birthday # Data urodzenia pacjenta
+    dataSet.PatientSex = gender # Plec pacjenta
+    dataSet.is_little_endian=True
+    dataSet.is_implicit_VR=True
+    dataSet.ContentDate = date  # Czas utworzenia pliku (YYYY:MM:DD)
+    dataSet.StudyDate = date    # Czas ostatniego otworzenia obrazu (YYYY-MM-DD)
+    dataSet.StudyTime = time    # Czas ostatniego otworzenia obrazu (HH:MM:SS)
+    dataSet.ContentTime=time    # Czas utworzenia pliku (HH:MM:SS)
+    dataSet.StudyInstanceUID = randomUId+"."+"2"   # Wygenerowany unikalny UID
+    dataSet.SeriesInstanceUID = randomUId+"."+"3"   # Wygenerowany unikalny UID
+    dataSet.SOPInstanceUID = randomUId+"."+"4"   # Wygenerowany unikalny UID
+    dataSet.SOPClassUID = "CT."+date+time   # Wygenerowany unikalny UID
+
+    dataSet.SamplesPerPixel = 1 # Liczba kanałów. 1 - dla skali szarosci
+    dataSet.PhotometricInterpretation = "MONOCHROME2" # MONOCHROE - obraz jest w skali szarości, 2 - maksymalna wartosc wskazuje kolor bialy
+    dataSet.PixelRepresentation = 0 # 0 - wartosci sa tylko dodatnie (unsigned) 1 - wartosci sa tez ujemne
+    dataSet.HighBit = 15    # Najważniejszy bit w pliku z obrazem
+    dataSet.BitsStored = 16 # Liczba bitow na jedna wartosc w obrazie
+    dataSet.BitsAllocated = 16  # Liczba bitow na jedna wartosc ktora jest zaalokowana dla obrazu
+    dataSet.SmallestImagePixelValue = b'\\x00\\x00' # Wskazanie minimalnej wartosci dla kanalu
+    dataSet.LargestImagePixelValue = b'\\xff\\xff'  # Wskazanie maksymalnej wartosci dla kanalu
+    dataSet.Rows = imageArray.shape[1]  # Liczba wierszy
+    dataSet.Columns = imageArray.shape[0]   # Liczba kolumn
+    if imageArray.dtype != np.uint16:   # Sprawdzenie czy wartosci sa w przedziale [0,255]
+        imageArray = skimage.img_as_uint(imageArray)    # Zamiana na wartosci w przedziale [0,255]
+        if transpose == True:   # Zamiana wierszy i kolumn (opcjonalne)
+            dataSet.Rows = imageArray.shape[0]
+            dataSet.Columns = imageArray.shape[1]
+    dataSet.PixelData = imageArray.tostring()   # Zapisanie obrazu
+    dataSet.save_as(filename)   # Zapisanie pliku na dysku
+
+
 def main():
     inData = data.imread("resources/in.png", as_grey=True)
 
@@ -101,6 +150,8 @@ def main():
     radonImage = radonTransform(inData, stepSize=step, detectorsNumber=detectorsNumber, detectorsWidth=detectorWidth)
     plt.imshow(radonImage, cmap='gray', extent=[0,180,len(radonImage),0], interpolation=None)
     inverseRadonImage = inverseRadonTransform(radonImage, stepSize=step, detectorsWidth=detectorWidth)
+    saveDicomFile("out.dcm", "Andrzej", "1234", "male", "20070304", inverseRadonImage)
+    #write_dicom(inverseRadonImage,'pretty.dcm')
     plt.subplot(2, 3, 4)
     plt.title("Inverse Radon transform image")
     plt.imshow(inverseRadonImage, cmap='gray')
